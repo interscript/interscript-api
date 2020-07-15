7ed03f5af8cd5c40daf346ba42ab8860634d1b3f
phuong <phuonghqh@gmail.com>
2020-07-15 15:55:22 +0700

    Add rubocop

--- lib/interscript-api/lambda_function.rb
+++ lib/interscript-api/lambda_function.rb
@@ -1,8 +1,47 @@
+require "interscript"
+require "json"
+
 module InterscriptApi
 
   class LambdaFunction
+
+    # @param [Object] event
+    # @param [Object] context
+    # @return [Hash{Symbol->Integer}]
     def self.process(event:, context:)
-      "Hello!"
+      body = JSON.parse event["body"]
+
+      system = body["system"]
+      input = body["input"]
+
+      puts "system: #{system}"
+      puts "input: #{input}"
+
+      # rs = { statusCode: 204 }
+
+      begin
+        result = Interscript.transliterate(system, input)
+
+        rs = {
+          statusCode: 200,
+          body: JSON.generate(
+            {
+              result: result,
+            },
+          ),
+        }
+      rescue => error
+        rs = {
+          statusCode: 500,
+          body: JSON.generate(
+            {
+              errorMessage: "InvalidSystemError: #{error.inspect}",
+            },
+          ),
+        }
+      end
+
+      rs
     end
   end
 end
