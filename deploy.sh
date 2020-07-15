3d99fda5f6f3002c9aa6842b0271b4748ceffa78
phuong <phuonghqh@gmail.com>
2020-07-15 13:16:55 +0700

    Add bundle

--- deploy.sh
+++ deploy.sh
@@ -1,1 +1,8 @@
+
+bundle config set path 'vendor/bundle'
 bundle install
+
+#bundle install --path vendor/bundle
+#[DEPRECATED] The `--path` flag is deprecated because it relies on being remembered across bundler invocations,
+# which bundler will no longer do in future versions.
+# Instead please use `bundle config set path 'vendor/bundle'`, and stop using this flag
