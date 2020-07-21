c897ea48afdf9bce389886a3fd251a358711f124
phuong <phuonghqh@gmail.com>
2020-07-22 00:49:33 +0700

    Update bundle config

--- build-prod.sh
+++ build-prod.sh
@@ -1,3 +1,7 @@
-rm -Rf vendor
-bundle install --deployment --without=test --quiet
+rm -Rf "$(pwd)/vendor"
+bundle config set path "$(pwd)/vendor/bundle"
 
+bundle install --without=test --gemfile="$(pwd)/Gemfile"
+
+#echo 123
+#echo "$(pwd)"
