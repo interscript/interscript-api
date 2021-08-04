17f0e0e26447c501ab4a9589a4e439f169005b74
phuong <phuonghqh@gmail.com>
2021-08-04 15:00:37 +0700

    Update build docker script to use env

--- .github/awsl-layer-docker/build.sh
+++ .github/awsl-layer-docker/build.sh
@@ -4,7 +4,7 @@ rm -Rf ./libs
 cp ../../Gemfile Gemfile
 
 docker build  --no-cache \
-  --build-arg INTERSCRIPT_GEM_VERSION=${INTERSCRIPT_GEM_VERSION} \
+  --build-arg INTERSCRIPT_GEM_VERSION=${GEM_VERSION} \
   -t interscript-lambda .
 
 id=$(docker create interscript-lambda)
